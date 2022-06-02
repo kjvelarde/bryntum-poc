b9d3589eae801f65e1d036335c594c39d7861dd1
kjvelarde <karlvelarde@gmail.com>
2022-06-02 13:29:01 -0400

    root

---
+++ src/app/app.component.ts
@@ -0,0 +1,51 @@
+/**
+ * App component script
+ */
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { BryntumResourceHistogramComponent, BryntumSchedulerProComponent } from '@bryntum/schedulerpro-angular';
+import { ResourceHistogram, SchedulerPro } from '@bryntum/schedulerpro/schedulerpro.lite.umd.js';
+
+import { histogramConfig, schedulerProConfig } from './app.config';
+
+@Component({
+    selector    : 'app-root',
+    templateUrl : './app.component.html'
+})
+
+export class AppComponent implements AfterViewInit {
+    schedulerProConfig = schedulerProConfig;
+    histogramConfig = histogramConfig;
+
+    schedulerPro: SchedulerPro;
+    resourceHistogram: ResourceHistogram;
+
+    @ViewChild(BryntumSchedulerProComponent) schedulerProComponent: BryntumSchedulerProComponent;
+    @ViewChild(BryntumResourceHistogramComponent) resourceHistogramComponent: BryntumResourceHistogramComponent;
+
+    ngAfterViewInit(): void {
+        this.schedulerPro = this.schedulerProComponent.instance;
+        this.resourceHistogram = this.resourceHistogramComponent.instance;
+        this.resourceHistogram.addPartner(this.schedulerPro);
+    }
+
+    /**
+     * handles clicks on toolbar checkboxes
+     */
+    onCheckboxAction({ source }, option: string): void {
+        this.resourceHistogram[option] = source.checked;
+    }
+
+    /**
+     * Handles zoom-in click event
+     */
+    onZoomIn(): void {
+        this.schedulerPro.zoomIn();
+    }
+
+    /**
+     * Handles zoom-out click event
+     */
+    onZoomOut(): void {
+        this.schedulerPro.zoomOut();
+    }
+}
